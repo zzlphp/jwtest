eb5cbe6e09b7625f91bf82344e959c2e531482b5
jw <jw@win>
2015-11-09 21:55:15 +0800

    kk

--- 1.php
+++ 1.php
@@ -5,4 +5,20 @@
 	function addsss(){
 		echo "hello words";
 	}
+
+
+	function adsds(){
+		echo "hello word";
+	}
+	function adswddsss(){
+		echo "hello words";
+	}
+
+	function addrs(){
+		echo "hello word";
+	}
+	function addshhss(){
+		echo "hello words";
+	}
+
 ?>
